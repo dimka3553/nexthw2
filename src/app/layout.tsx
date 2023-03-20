cf1480b42d7a0d32a48aee4aad808c0b2a0dbdb2
smoothM <37512400+dimka3553@users.noreply.github.com>
2023-03-20 14:59:48 +0100

    sticky navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-[#f4f6f9]">
-        <nav className="h-16  text-primary">
+        <nav className="h-16  text-primary sticky top-0 z-[11] bg-[#f4f6f9]">
           <div className="max-w-[1200px] px-5 items-center flex gap-5 justify-between h-full mx-auto">
             <div className="font-bold text-lg">
               <Link href="/">IMDB</Link>
